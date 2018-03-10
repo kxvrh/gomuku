72b651ada0d1c6d0d83bae6a16c22f6c70b19fdb
kxvrh <940546571@qq.com>
2018-03-11 02:10:16 +0800

    Update TreeNode.cpp

--- monte_carlo_tree_search/monte_carlo_tree_search/TreeNode.cpp
+++ monte_carlo_tree_search/monte_carlo_tree_search/TreeNode.cpp
@@ -2,13 +2,30 @@
 
 TreeNode* TreeNode::add_child_with_action(Action& new_action)
 {
-	TreeNode* child = new TreeNode(_state, new_action, this);
+	TreeNode* child = new TreeNode(_state, this);
+	child->_action = _action;
 	_children.push_back(child);
 	child->_state = _state.apply_action(new_action);
 	return child;
 }
 
-TreeNode * TreeNode::select()
+TreeNode * TreeNode::get_most_visited_child()
+{
+	int most_visited = -1;
+	TreeNode* most_visited_child = NULL;
+	for (int i = 0; i < _children.size(); i++)
+	{
+		TreeNode* node = this->get_child(i);
+		if (node->get_visited() > most_visited)
+		{
+			most_visited = node->get_visited;
+			most_visited_child = node;
+		}
+	}
+	return most_visited_child;
+}
+
+TreeNode * TreeNode::select(double uct_k)
 {
 	if (_children.empty())
 		return NULL;
@@ -20,7 +37,7 @@ TreeNode * TreeNode::select()
 		TreeNode* child = _children[i];
 		double uct_exploitation = _value / _visits + EPSILON;
 		double uct_exploration = sqrt(log(_visits) / child->get_visited() + EPSILON);
-		double uct_score = uct_exploitation + uct_exploration;
+		double uct_score = uct_exploitation + uct_k * uct_exploration;
 		if (uct_score > best_score)
 		{
 			best_score = uct_score;
@@ -34,7 +51,7 @@ TreeNode* TreeNode::expand()
 {
 	if (_actions.empty())
 	{
-		_state.get_actions(_actions);
+		_actions = _state.get_actions();
 	}
 	srand(time(0));
 	int random = rand() % _actions.size();
@@ -52,10 +69,23 @@ void TreeNode::simulate()
 	}
 }
 
-void TreeNode::update(vector<double>reward)
+void TreeNode::update(double reward)
 {
 	_visits++;
-	_value += reward[_state.get_player_id()];
+	_value += reward;
+}
+
+bool TreeNode::is_terminal() const
+{
+	if (_state.has_a_winner() != -1)
+		return true;
+	else
+		return false;
+}
+
+bool TreeNode::is_fully_expanded() const
+{
+	return _children.empty() == false && _children.size() == _actions.size();
 }
 
 
