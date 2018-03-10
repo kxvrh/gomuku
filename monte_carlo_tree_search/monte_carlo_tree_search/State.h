bc2818c41cec5296d949ee012b350c63af58004f
kxvrh <940546571@qq.com>
2018-03-11 02:09:51 +0800

    Update State.h

--- monte_carlo_tree_search/monte_carlo_tree_search/State.h
+++ monte_carlo_tree_search/monte_carlo_tree_search/State.h
@@ -3,13 +3,13 @@
 #include <vector>
 using std::vector;
 /*
-actions in 19x19 board
-342 343 ... 359 360
+actions in 15x15 board
+210 211 ... 223 224
 .   .       .   .
 .   .       .   .
-38  39  ...  55 56
-19  20  ...  36 37
-0    1  ...  17 18
+30  31  ...  43 44
+15  16  ...  28 29
+0    1  ...  13 14
 */
 
 #define SIZE 15
@@ -18,20 +18,30 @@ actions in 19x19 board
 class State
 {
 public:
-	void get_actions(vector<Action>& actions)//get all possible actions
-	{
-		for (int i = 0; i < GRID; i++)
-		{
-			if (board[i] == 0)
-				actions.push_back(Action(i, player_id));
-		}
-	}
+	State();
+	State& operator = (const State& other);
+	State(const State& other);
+
+	//get all possible actions
+	vector<Action>& get_actions();
+	//return false if no possible action found
+	bool get_random_action(Action& action);
 	State& apply_action(Action& new_action);
+
 	int get_player_id() { return player_id; }
+
+	bool is_over()const;
+	//return 1 if player wins, 0 if player loses, 0.5 if draw
+	double get_reward(int player)const;
+	//return player's id if player wins, -1 if no winner
 	int has_a_winner()const;
 private:
 	//1 for player1, 2 for player2
 	int player_id;
+	//occupied actions, 0 for unoccupied, 1 for occupied
+	vector<int>occupied;
+	//available actions, 0 for unavailable, 1 for available
+	int available[GRID];
 	//15x15 board
 	int board[GRID];
 };
