9dec622273321bf27bec7e093af8c7d9dd095128
kxvrh <940546571@qq.com>
2018-03-16 21:59:17 +0800

    Update state.cpp

--- monte_carlo_tree_search/monte_carlo_tree_search/state.cpp
+++ monte_carlo_tree_search/monte_carlo_tree_search/state.cpp
@@ -5,10 +5,37 @@ State& State::apply_action(Action& new_action)
 	int ind = new_action.get_ind();
 	if (available[ind] == 1)
 	{
-		occupied[ind] = 1;
+		occupied.push_back(ind);
 		available[ind] = 0;
+		board[ind] = player_id;
 	}
+	take_turn();
 	return *this;
+}
+
+void State::take_turn()
+{
+	if (player_id == 1)
+		player_id = 2;
+	else
+		player_id = 1;
+}
+
+void State::print_state()
+{
+	for (int i = 0; i < GRID; i++)
+	{
+		if (board[i] == 0)
+			std::cout << '-';
+		else if (board[i] == 1)
+			std::cout << "o";
+		else
+			std::cout << 'x';
+		std::cout << " ";
+		if ((i + 1) % SIZE == 0)
+			std::cout << std::endl;
+	}
+	std::cout << std::endl;
 }
 
 bool State::is_over() const
@@ -37,61 +64,81 @@ inline int State::has_a_winner() const
 	{
 		int ind = occupied[i];
 		int player = board[ind];
-		if (ind + 5 < SIZE)
+		int x = ind / SIZE;
+		int y = ind % SIZE;
+		if (x + 5 < SIZE)
 		{
 			int j = 0;
 			while (j < 5)
 			{
 				if (board[ind + j] != player)
 					break;
-				if (j = 4)
+				if (j == 4)
 					return player;
+				j++;
 			}
 		}
-		if (ind + 5 * SIZE < GRID)
+		if (y + 5 < SIZE)
 		{
 			int j = 0;
 			while (j < 5)
 			{
 				if (board[ind + j*SIZE] != player)
 					break;
-				if (j = 4)
+				if (j == 4)
 					return player;
+				j++;
 			}
 		}
-		if (ind + 5 * (SIZE + 1) < SIZE)
+		if (x + 5 < SIZE && y + 5 < SIZE)
 		{
 			int j = 0;
 			while (j < 5)
 			{
 				if (board[ind + j*(SIZE + 1)] != player)
 					break;
-				if (j = 4)
+				if (j == 4)
 					return player;
+				j++;
 			}
 		}
-		if (ind - 5 * (SIZE - 1) > 0)
+		if (x - 5 >= 0 && y + 5 < SIZE)
 		{
 			int j = 0;
 			while (j < 5)
 			{
-				if (board[ind - j*(SIZE - 1)] != player)
+				if (board[ind + j*(SIZE - 1)] != player)
 					break;
-				if (j = 4)
+				if (j == 4)
 					return player;
+				j++;
 			}
 		}
-		return -1;
 	}
+	return -1;
 }
 
 State::State()
 {
 	for (int i = 0; i < GRID; i++)
 	{
-		available[i] = 0;
+		available[i] = 1;
 		board[i] = 0;
 	}
+	player_id = 1;
+}
+
+State::State(int p)
+{
+	for (int i = 0; i < GRID; i++)
+	{
+		available[i] = 1;
+		board[i] = 0;
+	}
+	if (p == 1 || p == 2)
+		player_id = p;
+	else
+		std::cout << ("mistake!");
 }
 
 State & State::operator=(const State & other)
@@ -104,6 +151,7 @@ State & State::operator=(const State & other)
 	}
 	occupied = other.occupied;
 	player_id = other.player_id;
+	return *this;
 }
 
 State::State(const State & other)
@@ -111,25 +159,21 @@ State::State(const State & other)
 	*this = other;
 }
 
-vector<Action>& State::get_actions()
+void State::get_actions(vector<Action>&actions)
 {
-	vector<Action> actions;
 	for (int i = 0; i < GRID; i++)
 	{
 		if (available[i] == 1)
 		{
-			actions.push_back(Action(i, player_id));
-			available[i] = 0;
-			occupied[i] = player_id;
-			
+			actions.push_back(Action(i, player_id));	
 		}
 	}
-	return actions;
 }
 
 bool State::get_random_action(Action & action)
 {
-	vector<Action>actions = get_actions();
+	vector<Action>actions;
+	get_actions(actions);
 	if (actions.empty())
 		return false;
 	else
